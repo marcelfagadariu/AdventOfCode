af5ee76eb2d2e240f9b33e9939540bcb95b04911
Marcel Fagadariu <march_sb_16@yahoo.com>
2023-12-08 09:27:17 +0200

    first task done

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Marcel on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
